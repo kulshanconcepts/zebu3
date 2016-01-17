faabb597c87ed040e27860f0478e59bb3670069c
Tyrel Haveman <tyrel@kulshanconcepts.com>
2016-01-16 23:14:58 -0800

    Trying to load stuff from ATAGS but can't get it working in QEMU.
    I've ordered a real Pi to test out on instead.

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,26 +1,35 @@
 // Copyright (C) 2016 Kulshan Concepts. All rights reserved.
 // This is just something to play with. It should all be replaced with real stuff!
 
-#if !defined(__cplusplus)
-#include <stdbool.h>
-#endif
 #include <stddef.h>
 #include <stdint.h>
 
 #include "uart.h"
 #include "print.h"
+#include "atag.h"
   
-#if defined(__cplusplus)
-extern "C" /* Use C linkage for kernel_main */
-#endif
-void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
+extern "C"
+void kernel_main(uint32_t r0, uint32_t r1, uint32_t atagsAddress) {
 	(void) r0;
 	(void) r1;
-	(void) atags;
  
  	Uart uart;
-	kprintf("Hello, %s World! %d is a nice number; it's %X in hex.\r\n", "Zebu", 42, 42);
+	kprint("Zebu\r\n");
+	kprintf("Kernel is starting with %X %X %X\r\n", r0, r1, atagsAddress);
+
+	kprintf("Reading ATAG information from %X...", atagsAddress);
+
+	ATags atags(atagsAddress);
+	ATagDescriptor* atag;
+
+	while ((atag = atags.getNextTag()) != NULL) {
+		kprintf(" type %X\r\n", atag->tag);
+	}
+
+	kprint(" done\r\n");
  
+	kprint("\r\nEnd of execution\r\n");
+
 	// Just start echoing anything that's typed
 	while (true)
 		uart.putc(uart.getc());
