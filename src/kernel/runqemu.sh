5a9d444eae6f967a894307b09f2704531b7d99a0
Tyrel Haveman <tyrel@kulshanconcepts.com>
2016-01-17 13:09:28 -0800

    Make runqemu.sh smarter; can take argument "debug" now to do debug, otherwise not

--- src/kernel/runqemu.sh
+++ src/kernel/runqemu.sh
@@ -1,1 +1,16 @@
-../../bin/qemu-system-arm -kernel kernel.bin -initrd kernel.bin -cpu arm1176 -m 256 -M raspi -serial stdio -s -S
+if [ ! -e "kernel.bin" ]
+then
+	echo "You need to run 'make' first."
+	exit
+fi
+
+DO_DEBUG=
+if [ $# -ge 1 ] && [ $1 == "debug" ]
+then
+	DO_DEBUG="-s -S"
+	echo "QEMU will start with debugging. Attach GQB using ./debug.sh"
+	echo
+fi
+
+../../bin/qemu-system-arm -kernel kernel.bin -initrd kernel.bin -cpu arm1176 -m 256 -M raspi -serial stdio $DO_DEBUG
+
