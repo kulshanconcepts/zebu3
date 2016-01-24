b1625fc2f5a5a19f2d18a3968562b8b20405b2bf
Tyrel Haveman <tyrel@kulshanconcepts.com>
2016-01-23 22:46:20 -0800

    Add some support for send & recv messages; can pong (not tested)

--- src/sdpclient/sdp.cpp
+++ src/sdpclient/sdp.cpp
@@ -6,19 +6,6 @@
 
 #define MODULE "SdpClient"
 
-enum MessageType : uint8_t {
-    MESSAGE_PING = 0,
-    MESSAGE_PING_RESPONSE,
-    MESSAGE_GET_VERSION,
-    MESSAGE_VERSION_RESPONSE,
-    MESSAGE_LOG,
-    MESSAGE_REQUEST_KERNEL,
-    MESSAGE_FILE_INFO,
-    MESSAGE_FILE_DATA,
-    MESSAGE_ERROR,
-    MAX_MESSAGE
-};
-
 SdpClient::SdpClient(Serial& serial, Logger& logger) : serial(serial), logger(logger), stop(false), running(true) {
     logger.debug(MODULE, "Starting SDP processing thread");
     pthread_create(&thread, nullptr, startThread, (void*)this);
@@ -36,48 +23,35 @@ void* SdpClient::startThread(void* arg) {
 }
 
 void SdpClient::run() {
-    enum {
-        waitingForConnection,
-        readCommandStart,
-    } currentState = waitingForConnection;
-
     try {
         while (!stop) {
-            switch (currentState) {
-                case waitingForConnection:
-                    serial.waitForData();
-                    currentState = readCommandStart;
+            // TODO: This needs to be done a bit differently so someone can issue a command if they want while we're waiting
+
+            char signature[4];
+            readExactly(signature, 1);
+            if (signature[0] != 'Z') {
+                logger.debug(MODULE, "Not at command start, will keep looking (got %02X)", signature[0]);
+                continue;
+            }
+            readExactly(&signature[1], 3);
+            if (memcmp("Zebu", signature, 4) != 0) {
+                logger.warning(MODULE, "Expected protocol message signature, but got %02X %02X %02X %02X", signature[0], signature[1], signature[2], signature[3]);
+                continue;
+            }
+            uint8_t type;
+            readExactly(&type, 1);
+            logger.debug(MODULE, "Got message type %02X", (unsigned int)type);
+            if (type >= MAX_MESSAGE) {
+                logger.error(MODULE, "Received unknown message %02X", (unsigned int)type);
+                continue;
+            }
+
+            switch (type) {
+                case MESSAGE_PING:
+                    processPing();
                     break;
-                case readCommandStart:
-                    char signature[4];
-                    readExactly(signature, 1);
-                    if (signature[0] != 'Z') {
-                        logger.debug(MODULE, "Not at command start, will keep looking (got %02X)", signature[0]);
-                        currentState = waitingForConnection;
-                        continue;
-                    }
-                    readExactly(&signature[1], 3);
-                    if (memcmp("Zebu", signature, 4) != 0) {
-                        logger.warning(MODULE, "Expected protocol message signature, but got %02X %02X %02X %02X", signature[0], signature[1], signature[2], signature[3]);
-                        currentState = waitingForConnection;
-                        continue;
-                    }
-                    uint8_t type;
-                    readExactly(&type, 1);
-                    logger.debug(MODULE, "Got message type %02X", (unsigned int)type);
-                    if (type >= MAX_MESSAGE) {
-                        logger.error(MODULE, "Received unknown message %02X", (unsigned int)type);
-                        currentState = waitingForConnection;
-                        continue;
-                    }
-
-                    // TODO: process message
-                    logger.warning(MODULE, "Message parsing isn't implemented!");
-                    currentState = waitingForConnection;
-                    continue;
-
                 default:
-                    sleep(1);
+                    logger.warning(MODULE, "Message parsing isn't implemented for %02X!", (unsigned int)type);
             }
         }
     } catch (SerialException& ex) {
@@ -96,4 +70,71 @@ void SdpClient::readExactly(void* data, size_t length) {
         length -= br;
         dataPtr += br;
     }
+}
+
+void SdpClient::processPing() {
+    // no parameters expected
+    if (verifyRecvHash()) {
+        logger.info(MODULE, "Responding to ping from server");
+        SdpMessage message = startMessage(MESSAGE_PING_RESPONSE);
+        sendMessage(message);
+    }
+}
+
+SdpMessage SdpClient::startMessage(MessageType type) {
+    SdpMessage msg;
+    msg.type = type;
+
+    return msg;
+}
+
+void SdpClient::sendMessage(const SdpMessage& message) {
+    std::vector<uint8_t> data(message.data.size() + 5);
+    memcpy(data.data(), "Zebu", 4);
+    data.data()[4] = message.type;
+    memcpy(&(data.data()[5]), message.data.data(), message.data.size());
+    uint16_t hash = computeHash(data.data(), data.size());
+    data.push_back((uint8_t)(hash >> 8));
+    data.push_back((uint8_t)(hash & 0xFF));
+
+    serial.write((const char*)data.data(), data.size());
+}
+
+void SdpClient::resetRecvHash(MessageType type) {
+    recvHash = 0xFFFF;
+    addRecvHash((uint8_t)'Z');
+    addRecvHash((uint8_t)'e');
+    addRecvHash((uint8_t)'b');
+    addRecvHash((uint8_t)'u');
+    addRecvHash((uint8_t)type);
+}
+
+void SdpClient::addRecvHash(uint8_t datum) {
+    uint16_t x = recvHash >> 8 ^ datum;
+    x ^= x >> 4;
+    recvHash = (recvHash << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x << 5)) ^ ((uint16_t)x);
+}
+
+uint16_t SdpClient::computeHash(void* data, size_t length) {
+    uint16_t crc = 0xFFFF;
+    uint16_t x;
+    uint8_t* dataPtr = (uint8_t*)data;
+
+    while (length-- > 0) {
+        x = crc >> 8 ^ *dataPtr++;
+        x ^= x >> 4;
+        crc = (crc << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x << 5)) ^ ((uint16_t)x);
+    }
+
+    return crc;
+}
+
+bool SdpClient::verifyRecvHash() {
+    uint16_t wire;
+    readExactly(&wire, 2);
+    bool good = (wire == recvHash);
+    if (!good) {
+        logger.warning(MODULE, "CRC on received message not as expected. Data corrupt and ignored.");
+    }
+    return good;
 }
--- src/sdpclient/sdp.h
+++ src/sdpclient/sdp.h
@@ -4,8 +4,27 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <pthread.h>
+#include <vector>
 #include "serial.h"
 #include "logger.h"
+
+enum MessageType : uint8_t {
+    MESSAGE_PING = 0,
+    MESSAGE_PING_RESPONSE,
+    MESSAGE_GET_VERSION,
+    MESSAGE_VERSION_RESPONSE,
+    MESSAGE_LOG,
+    MESSAGE_REQUEST_KERNEL,
+    MESSAGE_FILE_INFO,
+    MESSAGE_FILE_DATA,
+    MESSAGE_ERROR,
+    MAX_MESSAGE
+};
+
+struct SdpMessage {
+    MessageType type;
+    std::vector<uint8_t> data;
+};
 
 class SdpClient {
 private:
@@ -15,11 +34,25 @@ private:
     pthread_mutex_t mutex;
     volatile bool stop;
     volatile bool running;
+    uint16_t recvHash;
 
     static void* startThread(void* arg);
     void run();
 
     void readExactly(void* data, size_t length);
+
+    SdpMessage startMessage(MessageType type);
+    void sendMessage(const SdpMessage& message);
+    uint16_t computeHash(void* data, size_t length);
+
+    void resetRecvHash(MessageType type);
+    void addRecvHash(uint8_t datum);
+    void addRecvHash(uint16_t datum);
+    void addRecvHash(uint32_t datum);
+    void addRecvHash(void* data, size_t length);
+    bool verifyRecvHash();
+
+    void processPing();
 
 public:
     SdpClient(Serial& serial, Logger& logger);
