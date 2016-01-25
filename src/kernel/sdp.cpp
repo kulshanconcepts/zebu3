a5e0b1773a599cb5c3689007c4edae3431afc246
Tyrel Haveman <tyrel@kulshanconcepts.com>
2016-01-24 20:03:47 -0800

    Add implementation for SdpServer. Something isn't right though.

--- src/kernel/sdp.cpp
+++ src/kernel/sdp.cpp
@@ -1,6 +1,9 @@
 // Copyright (C) 2016 Kulshan Concepts. All rights reserved.
 
 #include "sdp.h"
+#include "string.h"
+
+#define SDP_VERSION 1
 
 SdpServer::SdpServer(Uart& uart)
         : uart(uart) {
@@ -8,20 +11,188 @@ SdpServer::SdpServer(Uart& uart)
 }
 
 bool SdpServer::connect() {
-    // TODO: stuff
-    return false;
-}
+    ping();
 
-void SdpServer::log(uint8_t level, const char* module, const char* message) {
-    // TODO: stuff
-}
+    checkVersion();
 
-uint16_t SdpServer::computeHash(MessageType type, const void* data, size_t length) {
-    // TODO: stuff
-    return 0;
+    return true;
 }
 
 bool SdpServer::sendMessage(MessageType type, const void* data, size_t length) {
-    // TODO: stuff
-    return false;
+    uint16_t hash;
+    startHash(hash, type);
+
+    uart.putc('Z');
+    addHash(hash, 'Z');
+    uart.putc('e');
+    addHash(hash, 'e');
+    uart.putc('b');
+    addHash(hash, 'b');
+    uart.putc('u');
+    addHash(hash, 'u');
+    uart.putc(type);
+    addHash(hash, (uint8_t)type);
+
+    if (data != nullptr && length > 0) {
+        uart.write((const char*)data, length);
+        addHash(hash, (const uint8_t*)data, length);
+    }
+
+    uart.write((char*)&hash, sizeof(hash));
+
+    return true;
+}
+
+bool SdpServer::ping() {
+    if (!sendMessage(MESSAGE_PING, nullptr, 0)) {
+        return false;
+    }
+
+    MessageType responseType;
+    if (!getMessageStart(responseType)) {
+        return false;
+    }
+
+    if (responseType != MESSAGE_PING_RESPONSE) {
+        return false;
+    }
+
+    uint16_t hash;
+    startHash(hash, MESSAGE_PING_RESPONSE);
+    if (get16() != hash) {
+        return false;
+    }
+
+    return true;
+}
+
+bool SdpServer::checkVersion() {
+    uint16_t ourVersion = SDP_VERSION;
+    if (!sendMessage(MESSAGE_GET_VERSION, &ourVersion, sizeof(ourVersion))) {
+        return false;
+    }
+
+    MessageType responseType;
+    if (!getMessageStart(responseType)) {
+        return false;
+    }
+
+    if (responseType != MESSAGE_VERSION_RESPONSE) {
+        return false;
+    }
+
+    uint16_t hash;
+    startHash(hash, MESSAGE_VERSION_RESPONSE);
+
+    uint16_t theirVersion = get16();
+    addHash(hash, theirVersion);
+
+    if (get16() != hash) {
+        return false;
+    }
+
+    if (theirVersion != SDP_VERSION) {
+        return false;
+    }
+
+    return true;
+}
+
+void SdpServer::log(uint8_t level, const char* module, const char* message) {
+    const size_t bufferLength = 576;
+    char buffer[bufferLength];
+
+    buffer[0] = level;
+    uint16_t modLen = strlen(module);
+    memcpy(&buffer[1], &modLen, sizeof(modLen));
+    memcpy(&buffer[3], module, modLen);
+
+    uint16_t msgLen = strlen(message);
+    memcpy(&buffer[3 + modLen], &msgLen, sizeof(msgLen));
+    memcpy(&buffer[5 + modLen], message, msgLen);
+
+    sendMessage(MESSAGE_LOG, buffer, 5 + modLen + msgLen);
+}
+
+void SdpServer::startHash(uint16_t& crc, MessageType type) {
+    crc = 0xFFFF;
+    addHash(crc, 'Z');
+    addHash(crc, 'e');
+    addHash(crc, 'b');
+    addHash(crc, 'u');
+    addHash(crc, (uint8_t)type);
+}
+
+void SdpServer::addHash(uint16_t& crc, uint32_t datum) {
+	addHash(crc, (uint8_t)(datum & 0xFF));
+	addHash(crc, (uint8_t)(datum >> 8));
+	addHash(crc, (uint8_t)(datum >> 16));
+    addHash(crc, (uint8_t)(datum >> 24));
+}
+
+void SdpServer::addHash(uint16_t& crc, uint16_t datum) {
+	addHash(crc, (uint8_t)(datum & 0xFF));
+	addHash(crc, (uint8_t)(datum >> 8));
+}
+
+void SdpServer::addHash(uint16_t& crc, uint8_t datum) {
+    uint16_t x = crc >> 8 ^ datum;
+    x ^= x >> 4;
+    crc = (crc << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x << 5)) ^ ((uint16_t)x);
+}
+
+void SdpServer::addHash(uint16_t& crc, const uint8_t* data, uint16_t length) {
+    addHash(crc, length);
+    while (length--) {
+        addHash(crc, *data++);
+    }
+}
+
+bool SdpServer::getMessageStart(MessageType& type) {
+    while (true) {
+        uint8_t b = uart.getc();
+        if (b != 'Z') {
+            return false;
+        }
+
+        b = uart.getc();
+
+        if (b != 'e') {
+            return false;
+        }
+
+        b = uart.getc();
+
+        if (b != 'b') {
+            return false;
+        }
+
+        b = uart.getc();
+
+        if (b != 'u') {
+            return false;
+        }
+
+        type = (MessageType) uart.getc();
+
+        return true;
+    }
+}
+
+uint8_t SdpServer::get8() {
+    return uart.getc();
+}
+
+uint16_t SdpServer::get16() {
+    return uart.getc() | ((uint16_t)uart.getc() << 8);
+}
+
+uint32_t SdpServer::get32() {
+    return uart.getc() | ((uint32_t)uart.getc() << 8) | ((uint32_t)uart.getc() << 16) | ((uint32_t)uart.getc() << 24);
+}
+
+void SdpServer::get(uint8_t* data, uint16_t length) {
+    while (length--) {
+        *data++ = uart.getc();
+    }
 }
--- src/kernel/sdp.h
+++ src/kernel/sdp.h
@@ -25,7 +25,22 @@ class SdpServer {
 private:
     Uart& uart;
     bool sendMessage(MessageType type, const void* data, size_t length);
-    uint16_t computeHash(MessageType type, const void* data, size_t length);
+    bool ping();
+    bool checkVersion();
+
+    bool getMessageStart(MessageType& type);
+    uint8_t get8();
+    uint16_t get16();
+    uint32_t get32();
+    void get(uint8_t* data, uint16_t length);
+
+    void startHash(uint16_t& crc, MessageType type);
+    inline void addHash(uint16_t& crc, char letter) { addHash(crc, (uint8_t)letter); }
+    void addHash(uint16_t& crc, uint8_t datum);
+    void addHash(uint16_t& crc, uint16_t datum);
+    void addHash(uint16_t& crc, uint32_t datum);
+    void addHash(uint16_t& crc, const uint8_t* data, uint16_t length);
+    inline void addHash(uint16_t& crc, const char* string, uint16_t length) { addHash(crc, (uint8_t*)string, length); }
 
 public:
     SdpServer(Uart& uart);
