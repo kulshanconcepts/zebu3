6a32b19c8ee6ccff06100af40b7cca5fea7ad5e7
Tyrel Haveman <tyrel@kulshanconcepts.com>
2016-01-26 22:23:12 -0800

    See if adding types helps

--- kernel/shared-boot/include/sys/_types.h
+++ kernel/shared-boot/include/sys/_types.h
@@ -2,6 +2,12 @@
 #pragma #ifndef __TYPES_H_
 #define __TYPES_H_
 
+#define _TYPE_size_t unsigned int
+#define _TYPE_wchar_t unsigned short
+#define _TYPE_ptrdiff_t unsigned int
+#define __INT16_TYPE__ short int
+#define __INT32_TYPE__ int
+#define __INT64_TYPE__ long int
 
 
 #endif /* end of include guard: __TYPES_H_ */
