20dd8a9656afd62295690de7caada4c19e3acbaa
Tyrel Haveman <tyrel@kulshanconcepts.com>
2016-01-29 20:59:27 -0800

    Format code better

--- kernel/kernel/src/kernel.cpp
+++ kernel/kernel/src/kernel.cpp
@@ -114,5 +114,7 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t atagsAddress) {
 
 	exceptions.enableExceptions();
 
-	while (true) { asm("wfi"); } // idle forever
+	while (true) {
+		asm("wfi");
+	} // idle forever
 }
